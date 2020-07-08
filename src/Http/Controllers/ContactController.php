b5299b15dfcd78dc242c94ae0650f9fa11cfd08d
Romel Niño Paano <rpaano02@gmail.com>
2020-07-08 21:21:03 +0800

    Add Contact model and migration

--- src/Http/Controllers/ContactController.php
+++ src/Http/Controllers/ContactController.php
@@ -14,6 +14,7 @@ class ContactController extends Controller
 
     public function store(Request $request)
     {
-        return $request->all();
+
+        return ;
     }
 }
---
+++ src/Models/Contact.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RomelPaano\Contact\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    //
+}
